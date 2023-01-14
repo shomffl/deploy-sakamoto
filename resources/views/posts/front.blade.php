161fdf3827517ab19a3ebe5e6d5228a084700456
MakotoSakamoto <Makoto20020523@gmail.com>
2023-01-15 01:34:35 +0900

    kakka

---
+++ resources/views/posts/front.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>aioeo</title>
+        
+    </head>
+    <body class="antialiased">
+        <h1>野球実況.com</h1>
+        <h1>ルーム一覧</h1>
+        
+    </body>
+</html>
